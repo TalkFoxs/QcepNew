bcc55907ed37f01f88e76cced5fcfd5f30852eef
bisbe00 <bisbe00@gmail.com>
2023-11-16 19:08:01 +0100

    fin, base de dades

--- data_base_creation.sql
+++ data_base_creation.sql
@@ -27,14 +27,6 @@ create table document(
     foreign key(proces_id) references proces(id)
 );
 
--- grupInteres 0,1 : 0,N Sortida
--- grupInteres 0,1 : 0,N proveidor
-create table grupInteres(
-	id int primary key auto_increment,
-	nom varchar(50) not null,
-    descripcio varchar(300) not null
-);
-
 create table usuari(
 	id int primary key auto_increment,
 	username varchar(50) not null,
@@ -81,14 +73,69 @@ create table organitzacio(
     logo varchar(100) not null
 );
 
+-- grupInteres 0,1 : 0,N Sortida
+-- grupInteres 0,1 : 0,N proveidor
+create table grupInteres(
+	id int primary key auto_increment,
+	nom varchar(50) not null,
+    descripcio varchar(300) not null
+);
+
 -- proveidor/client: qui dona dada al proces
 -- nom i clau
+create table proveidor_client(
+	nom varchar(50) not null,
+    clau varchar(50) not null
+);
 
--- sortida: 
--- nom i clau
+-- grupInteres 0,1 : 0,N proveidor/client
+create table grup_te_proveidor(
+	grup_id int not null,
+    nom varchar(50) not null,
+    primary key (grup_id,nom),
+    foreign key (grup_id) references grupInteres(id),
+    foreign key (nom) references proveidor_client(nom)
+);
+
+-- proces N : N proveidor/client
+create table proces_proveidor_client(
+	nom varchar(50) not null,
+    proces_id int not null,
+    primary key (nom, proces_id),
+    foreign key (nom) references proveidor_client(nom),
+    foreign key (proces_id) references proces(id)
+);
 
 -- punt de la norma
 -- primerNum i segundaNum
+create table puntNorma(
+	primerNum int not null,
+    segundaNum int not null
+);
+
+-- proces N : N puntNorma
+create table puntNorma_proces(
+	num1 int not null,
+    num2 int not null,
+    proces_id int not null,
+    primary key (num1,num2,proces_id),
+    foreign key (num1) references puntNorma(primerNum),
+    foreign key (num2) references puntNorma(segundaNum),
+    foreign key (proces_id) references proces(id)
+);
 
 -- recurs
 -- tipus i descripció
+create table recurs(
+	tipus varchar(100) not null,
+    descripcio varchar(300) not null
+);
+
+-- proces N : N recurs
+create table necessita(
+	recus_tipus varchar(100) not null,
+    proces_id int not null,
+    primary key (recurs_tipus,proces_id),
+    foreign key (recurs_tipus) references recurs(tipus),
+    foreign key (proces_id) references proces(id)
+);
