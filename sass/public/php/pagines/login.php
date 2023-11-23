23791266d7f26ef567f654bd473d845d2c8d6913
bisbe00 <bisbe00@gmail.com>
2023-11-23 20:11:52 +0100

    login

---
+++ sass/public/php/pagines/login.php
@@ -0,0 +1,60 @@
+<?php
+
+function sanitize($data){
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST["login"])){
+    $mail = sanitize($_POST["mail"]);
+    $pass = sanitize($_POST["pass"]);
+    
+    if (empty($mail)) {
+        $error['mail'] = "put your email";
+        unset($mail);
+    } elseif (! filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $error['mail'] = "incorrect format";
+        unset($mail);
+    }
+    
+    if(empty($pass)){
+        $error['pass'] = "the password";
+        unset($pass);
+    }
+    
+    if(empty($error)){
+        //database
+        
+        //compare the database information and the inputs of the form if there the same
+        
+        //if does not exist, display the message $error to both inputs
+        
+        //else, header(Location: 'proces.php);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" placeholder="email" id="mail" name="mail">
+        <br>
+       <span><?php if (isset($error['mail'])) { echo $error['mail']; } ?></span>
+        <input type="password" placeholder="password" id="pass" name="pass">
+        <br>
+        <?php  if (isset($error['pass'])) { echo $error['pass']; }?>
+        <input type="submit" id="login" name="login" value="Login">
+        <br>
+    </form>
+    <p><a href="">Create an account</a></p>
+</body>
+</html>
