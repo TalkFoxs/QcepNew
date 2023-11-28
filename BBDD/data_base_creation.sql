c37ae03233e3bb11109f093b77a282e05505f672
bisbe00 <bisbe00@gmail.com>
2023-11-28 19:19:41 +0100

    fet, fin, crear les taules de base de dates

--- BBDD/data_base_creation.sql
+++ BBDD/data_base_creation.sql
@@ -3,36 +3,52 @@ create database qcep character set utf8mb4 collate utf8mb4_spanish_ci;
 
 use qcep;
 
+<<<<<<< HEAD
 -- process N : N Recurs
 -- proces N : N punt de la norma
 -- proces N : N sortida
 -- proces N : N proveidor
+=======
+create table usuari(
+	username varchar(100)primary key,
+    email varchar(100) not null,
+    es_administrador boolean not null
+);
+
+create table puntNorma(
+	primerNum int not null,
+    segundaNum int not null,
+    primary key(primerNum,segundaNum)
+);
+
+>>>>>>> c6cd8c9 (fet, fin, crear les taules de base de dates)
 create table proces (
-	id int auto_increment,
-	nom varchar(100) not null,
+	nom varchar(100) primary key,
     tipus varchar(100) not null,
     objectiu varchar(300) not null,
     usuari_username varchar(100) not null,
     foreign key(usuari_username) references usuari(username)
 );
 
+create table proces_te_puntNorma(
+	num1 int not null,
+    num2 int not null,
+    proces_nom varchar(100) not null,
+    primary key (num1,num2,proces_nom),
+	foreign key(num1, num2) REFERENCES puntNorma(primerNum, segundaNum),
+    foreign key (proces_nom) references proces(nom)
+);
+
 create table document(
-	id int auto_increment,
-	nom varchar(100) not null,
+	nom varchar(100) primary key,
     tipus varchar(100) not null,
     link varchar(200) not null,
     proces_nom varchar(100) not null,
     foreign key(proces_nom) references proces(nom)
 );
 
-create table usuari(
-	username varchar(100) not null,
-    email varchar(100) not null,
-    es_administrador boolean not null
-);
-
 create table avaluacio(
-	tipus varchar(100) not null,
+	tipus varchar(100) not null primary key,
     nivell varchar(100) not null,
     valoracio varchar(200) not null,
     planificacio varchar(100) not null,
@@ -41,7 +57,7 @@ create table avaluacio(
 );
 
 create table avaluacions(
-	avaluacio_tipus varchar(200) primary key,
+	avaluacio_tipus varchar(200) not null primary key,
     usuari_username varchar(100) not null,
     proces_nom varchar(100) not null,
     foreign key(avaluacio_tipus) references avaluacio(tipus),
@@ -50,7 +66,7 @@ create table avaluacions(
 );
 
 create table indicador(
-	codi varchar(50) not null,
+	codi varchar(50) not null primary key,
     nom varchar(100) not null,
     link varchar(200) not null,
     curs varchar(50) not null,
@@ -60,8 +76,12 @@ create table indicador(
 );
 
 create table organitzacio(
+<<<<<<< HEAD
 	nom varchar(50) not null,
 >>>>>>> fe289f7 (base de dades)
+=======
+	nom varchar(50) not null primary key,
+>>>>>>> c6cd8c9 (fet, fin, crear les taules de base de dates)
     email varchar(100) not null,
     web varchar(100) not null,
     logo varchar(100) not null
@@ -70,7 +90,7 @@ create table organitzacio(
 -- grupInteres 0,1 : 0,N Sortida
 -- grupInteres 0,1 : 0,N proveidor
 create table grupInteres(
-	nom varchar(100) not null,
+	nom varchar(100) not null primary key,
     descripcio varchar(300) not null
 );
 
@@ -92,27 +112,12 @@ create table client(
     foreign key(proces_nom) references proces(nom)
 );
 
-create table puntNorma(
-	primerNum int not null,
-    segundaNum int not null,
-    primary key(primerNum,segundaNum)
-);
-
-create table proces_te_puntNorma(
-	num1 int not null,
-    num2 int not null,
-    proces_nom varchar(100) not null,
-    primary key (num1,num2,proces_id),
-    foreign key (num1) references puntNorma(primerNum),
-    foreign key (num2) references puntNorma(segundaNum),
-    foreign key (proces_nom) references proces(nom)
-);
-
 create table recurs(
-	nom varchar(100) not null,
+	nom varchar(100) not null primary key,
     tipus varchar(300) not null
 );
 
+<<<<<<< HEAD
 -- proces N : N recurs
 create table necessita(
 	recus_tipus varchar(100) not null,
@@ -120,4 +125,19 @@ create table necessita(
     primary key (recurs_tipus,proces_id),
     foreign key (recurs_tipus) references recurs(tipus),
     foreign key (proces_id) references proces(id)
+=======
+create table proces_necessita_recurs(
+	recurs_nom varchar(100) not null,
+    proces_nom varchar(100) not null,
+    primary key (recurs_nom,proces_nom),
+    foreign key (recurs_nom) references recurs(nom),
+    foreign key (proces_nom) references proces(nom)
+);
+
+create table apartat(
+	nom varchar(100) not null primary key,
+    icona varchar(100) not null,
+    descripcio varchar(200) not null,
+    link varchar(100) not null
+>>>>>>> c6cd8c9 (fet, fin, crear les taules de base de dates)
 );
