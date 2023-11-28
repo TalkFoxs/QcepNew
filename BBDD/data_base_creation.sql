64bb2334bd6242730c4353fca4cb8eac29c75f85
TalkFoxs <2002duguxiu@gmail.com>
2023-11-28 19:08:10 +0100

    Corregir errores

--- BBDD/data_base_creation.sql
+++ BBDD/data_base_creation.sql
@@ -4,6 +4,7 @@ create database qcep character set utf8mb4 collate utf8mb4_spanish_ci;
 use qcep;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 -- process N : N Recurs
 -- proces N : N punt de la norma
 -- proces N : N sortida
@@ -17,10 +18,37 @@ create table usuari(
 
 create table puntNorma(
 	primerNum int not null,
+=======
+-- 先创建没有外键依赖的表
+create table usuari(
+    username varchar(100) not null,
+    email varchar(100) not null,
+    es_administrador boolean not null,
+    primary key (username)
+);
+
+create table organitzacio(
+    nom varchar(50) not null,
+    email varchar(100) not null,
+    web varchar(100) not null,
+    logo varchar(100) not null,
+    primary key (nom)
+);
+
+create table grupInteres(
+    nom varchar(100) not null,
+    descripcio varchar(300) not null,
+    primary key (nom)
+);
+
+create table puntNorma(
+    primerNum int not null,
+>>>>>>> 9b3d7de (Corregir errores)
     segundaNum int not null,
     primary key(primerNum,segundaNum)
 );
 
+<<<<<<< HEAD
 >>>>>>> c6cd8c9 (fet, fin, crear les taules de base de dates)
 create table proces (
 	nom varchar(100) primary key,
@@ -49,29 +77,71 @@ create table document(
 
 create table avaluacio(
 	tipus varchar(100) not null primary key,
+=======
+create table recurs(
+    nom varchar(100) not null,
+    tipus varchar(300) not null,
+    primary key (nom)
+);
+
+create table avaluacio(
+    tipus varchar(100) not null,
+>>>>>>> 9b3d7de (Corregir errores)
     nivell varchar(100) not null,
     valoracio varchar(200) not null,
     planificacio varchar(100) not null,
     accions varchar(300) not null,
-    estrategia varchar(100) not null
+    estrategia varchar(100) not null,
+    primary key (tipus)
+);
+
+-- 然后创建有外键依赖的表
+create table proces (
+    id int auto_increment,
+    nom varchar(100) not null,
+    tipus varchar(100) not null,
+    objectiu varchar(300) not null,
+    usuari_username varchar(100) not null,
+    primary key (id),
+    foreign key(usuari_username) references usuari(username)
+);
+
+create table document(
+    id int auto_increment,
+    nom varchar(100) not null,
+    tipus varchar(100) not null,
+    link varchar(200) not null,
+    proces_nom varchar(100) not null,
+    primary key (id),
+    foreign key(proces_nom) references proces(nom)
 );
 
 create table avaluacions(
+<<<<<<< HEAD
 	avaluacio_tipus varchar(200) not null primary key,
+=======
+    avaluacio_tipus varchar(100) not null,
+>>>>>>> 9b3d7de (Corregir errores)
     usuari_username varchar(100) not null,
     proces_nom varchar(100) not null,
+    primary key(avaluacio_tipus, usuari_username, proces_nom),
     foreign key(avaluacio_tipus) references avaluacio(tipus),
     foreign key(usuari_username) references usuari(username),
     foreign key(proces_nom) references proces(nom)
 );
 
 create table indicador(
+<<<<<<< HEAD
 	codi varchar(50) not null primary key,
+=======
+    codi varchar(50) not null,
+>>>>>>> 9b3d7de (Corregir errores)
     nom varchar(100) not null,
     link varchar(200) not null,
     curs varchar(50) not null,
     valoracio varchar(100) not null,
     proces_nom varchar(100) not null,
+<<<<<<< HEAD
 	foreign key(proces_nom) references proces(nom)
 );
 
@@ -92,17 +162,20 @@ create table organitzacio(
 create table grupInteres(
 	nom varchar(100) not null primary key,
     descripcio varchar(300) not null
+=======
+    primary key (codi),
+    foreign key(proces_nom) references proces(nom)
+>>>>>>> 9b3d7de (Corregir errores)
 );
 
 create table proveidor(
-	grup_nom varchar(100) not null,
+    grup_nom varchar(100) not null,
     proces_nom varchar(100) not null,
     entrada varchar(300) not null,
     primary key(grup_nom,proces_nom),
     foreign key(grup_nom) references grupInteres(nom),
     foreign key(proces_nom) references proces(nom)
 );
-
 create table client(
 	grup_nom varchar(100) not null,
     proces_nom varchar(100) not null,
@@ -139,5 +212,9 @@ create table apartat(
     icona varchar(100) not null,
     descripcio varchar(200) not null,
     link varchar(100) not null
+<<<<<<< HEAD
 >>>>>>> c6cd8c9 (fet, fin, crear les taules de base de dates)
 );
+=======
+);
+>>>>>>> 9b3d7de (Corregir errores)
