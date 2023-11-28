9b3d7de5058cfb625a24cd9b94e598ffcbaeb24c
TalkFoxs <2002duguxiu@gmail.com>
2023-11-28 19:08:10 +0100

    Corregir errores

--- BBDD/data_base_creation.sql
+++ BBDD/data_base_creation.sql
@@ -3,79 +3,100 @@ create database qcep character set utf8mb4 collate utf8mb4_spanish_ci;
 
 use qcep;
 
-create table proces (
-	id int auto_increment,
-	nom varchar(100) not null,
-    tipus varchar(100) not null,
-    objectiu varchar(300) not null,
-    usuari_username varchar(100) not null,
-    foreign key(usuari_username) references usuari(username)
-);
-
-create table document(
-	id int auto_increment,
-	nom varchar(100) not null,
-    tipus varchar(100) not null,
-    link varchar(200) not null,
-    proces_nom varchar(100) not null,
-    foreign key(proces_nom) references proces(nom)
-);
-
+-- 先创建没有外键依赖的表
 create table usuari(
-	username varchar(100) not null,
+    username varchar(100) not null,
     email varchar(100) not null,
-    es_administrador boolean not null
+    es_administrador boolean not null,
+    primary key (username)
+);
+
+create table organitzacio(
+    nom varchar(50) not null,
+    email varchar(100) not null,
+    web varchar(100) not null,
+    logo varchar(100) not null,
+    primary key (nom)
+);
+
+create table grupInteres(
+    nom varchar(100) not null,
+    descripcio varchar(300) not null,
+    primary key (nom)
+);
+
+create table puntNorma(
+    primerNum int not null,
+    segundaNum int not null,
+    primary key(primerNum,segundaNum)
+);
+
+create table recurs(
+    nom varchar(100) not null,
+    tipus varchar(300) not null,
+    primary key (nom)
 );
 
 create table avaluacio(
-	tipus varchar(100) not null,
+    tipus varchar(100) not null,
     nivell varchar(100) not null,
     valoracio varchar(200) not null,
     planificacio varchar(100) not null,
     accions varchar(300) not null,
-    estrategia varchar(100) not null
+    estrategia varchar(100) not null,
+    primary key (tipus)
+);
+
+-- 然后创建有外键依赖的表
+create table proces (
+    id int auto_increment,
+    nom varchar(100) not null,
+    tipus varchar(100) not null,
+    objectiu varchar(300) not null,
+    usuari_username varchar(100) not null,
+    primary key (id),
+    foreign key(usuari_username) references usuari(username)
+);
+
+create table document(
+    id int auto_increment,
+    nom varchar(100) not null,
+    tipus varchar(100) not null,
+    link varchar(200) not null,
+    proces_nom varchar(100) not null,
+    primary key (id),
+    foreign key(proces_nom) references proces(nom)
 );
 
 create table avaluacions(
-	avaluacio_tipus varchar(200) primary key,
+    avaluacio_tipus varchar(100) not null,
     usuari_username varchar(100) not null,
     proces_nom varchar(100) not null,
+    primary key(avaluacio_tipus, usuari_username, proces_nom),
     foreign key(avaluacio_tipus) references avaluacio(tipus),
     foreign key(usuari_username) references usuari(username),
     foreign key(proces_nom) references proces(nom)
 );
 
 create table indicador(
-	codi varchar(50) not null,
+    codi varchar(50) not null,
     nom varchar(100) not null,
     link varchar(200) not null,
     curs varchar(50) not null,
     valoracio varchar(100) not null,
     proces_nom varchar(100) not null,
-	foreign key(proces_nom) references proces(nom)
-);
-
-create table organitzacio(
-	nom varchar(50) not null,
-    email varchar(100) not null,
-    web varchar(100) not null,
-    logo varchar(100) not null
-);
-
-create table grupInteres(
-	nom varchar(100) not null,
-    descripcio varchar(300) not null
+    primary key (codi),
+    foreign key(proces_nom) references proces(nom)
 );
 
 create table proveidor(
-	grup_nom varchar(100) not null,
+    grup_nom varchar(100) not null,
     proces_nom varchar(100) not null,
     entrada varchar(300) not null,
     primary key(grup_nom,proces_nom),
     foreign key(grup_nom) references grupInteres(nom),
     foreign key(proces_nom) references proces(nom)
 );
-
 create table client(
 	grup_nom varchar(100) not null,
     proces_nom varchar(100) not null,
