b602ef6b4461624623db1d975f07e26f7135c0ac
bisbe00 <122985262+bisbe00@users.noreply.github.com>
2023-12-11 21:11:35 +0100

    Update data_base_creation.sql

--- BBDD/data_base_creation.sql
+++ BBDD/data_base_creation.sql
@@ -4,9 +4,9 @@ create database qcep character set utf8mb4 collate utf8mb4_spanish_ci;
 use qcep;
 
 create table usuari(
-	username varchar(100)primary key,
-    password varchar(200) not null,
-    email varchar(100) not null,
+	email varchar(200)primary key,
+    password varchar(100) not null,
+    username varchar(50) not null,
     es_administrador boolean not null
 );
 
